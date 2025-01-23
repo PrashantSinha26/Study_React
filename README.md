9cb67e62fc5c6ea20ae74d274d12a4a18f8d73fd
PrashantSinha26 <sinhaprashant2016@gmail.com>
2025-01-23 20:27:44 +0530

    Episode2React

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
- #NAMASTE_REACT
+ # NAMASTE_REACT
+
+ # Parcel
+ - Dev Build
+ - Local Server
+ - HMR [Hot Module Replace]
+ - File Watching Algorithum - written in C++
+ - Caching - Faster Builds
+ - Image Optimization
+ - Bundling
+ - Compressing
+ - Consistent Hashing
+ - Code Spliting
+ - Differential Bundling
+ - Diagnostic
+ - Error Handling
+ - HTTPs
+ - Tree Shaking Algorithum
--- app.js
+++ app.js
@@ -19,11 +19,13 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent); */
 
 // If a parent have a sibling or having a child node more than 1 then we wrap up the child in an "ARRAY"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent =
  React.createElement("div",{id:"parent"},
     React.createElement("div",{id:"child"},[
-        React.createElement("h1",{},"I am H1 Heading"),
+        React.createElement("h1",{},"I am BIG Heading"),
         React.createElement("h2",{},"I am H2 Heading"),
     ])
 );
